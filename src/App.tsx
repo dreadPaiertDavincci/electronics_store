4a1f9c40f3274d9b168e2e7e3dfa5b010d56afd9
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-11 15:56:31 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ function App() {
     <div className="App">
       <h1>Welcome to My App</h1>
       <p>This is a simple React application.</p>
+            <p>This is a simple React application.</p>
+
     </div>
   );
 }
