6be1dcb2c0523c8f22ffbebb24bc2816fbb99821
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-12 18:31:13 +0300

    asd

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,6 +1,120 @@
+import { useEffect, useRef, type ReactNode } from "react";
+import "../Style/Home.css";
 function Home() {
-    return(
-        <h1>Hello in Home Page</h1>
+  type ImageSliderInfoType = {
+    Image: string;
+    Title: string;
+    Description: string;
+    seeMoreBtn: ReactNode;
+  };
+  const ImageSliderInfo: ImageSliderInfoType[] = [
+    {
+      Image: "../../src/HomeImage/C1.png",
+      Title: "watch",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+    {
+      Image: "../../src/HomeImage/C2.png",
+      Title: "Airpod",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+    {
+      Image: "../../src/HomeImage/C3.png",
+      Title: "XBOX",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+    {
+      Image: "../../src/HomeImage/C4.png",
+      Title: "Iphone",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+    {
+      Image: "../../src/HomeImage/C5.png",
+      Title: "LAPTOP",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+    {
+      Image: "../../src/HomeImage/C6.png",
+      Title: "computer screen",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.",
+      seeMoreBtn: <button className="seeMore">SEE MORE &#8599;</button>,
+    },
+  ];
+
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const listRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    const listHTML = listRef.current;
+    if (!carousel || !listHTML) return;
+
+    const showSlider = (type: "next" | "prev") => {
+      carousel.classList.remove("next", "prev");
+      const items = listHTML.querySelectorAll(".item");
+      if (type === "next") {
+        listHTML.appendChild(items[0]);
+        carousel.classList.add("next");
+      } else {
+        listHTML.prepend(items[items.length - 1]);
+        carousel.classList.add("prev");
+      }
+    };
+
+    const seeMoreButtons = listHTML.querySelectorAll(".seeMore");
+    const onSeeMoreClick = () => {
+      carousel.classList.remove("next", "prev");
+      carousel.classList.add("showDetail");
+    };
+    seeMoreButtons.forEach((btn) =>
+      btn.addEventListener("click", onSeeMoreClick)
     );
+
+    const interval = setInterval(() => {
+      showSlider("next");
+    }, 3000);
+
+    return () => {
+      seeMoreButtons.forEach((btn) =>
+        btn.removeEventListener("click", onSeeMoreClick)
+      );
+      clearInterval(interval);
+    };
+  }, []);
+
+  const LoopSliderHome = ImageSliderInfo.map((element, index) => {
+    return (
+      <div className="item" key={index}>
+        <img src={element.Image} alt={element.Title} />
+        <div className="introduce" key={index}>
+          <div className="topic">{element.Title}</div>
+          <div className="des">{element.Description}</div>
+          {element.seeMoreBtn}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <section className="HomeSection">
+      <div className="carousel" ref={carouselRef}>
+        <div className="list" ref={listRef}>
+          {LoopSliderHome}
+        </div>
+      </div>
+    </section>
+  );
 }
+
 export default Home;
