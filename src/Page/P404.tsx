4702a0d360e1bbd28b470cca2ad56377e07c86d0
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-13 21:27:54 +0300

    asd

--- src/Page/P404.tsx
+++ src/Page/P404.tsx
@@ -1,13 +1,15 @@
-import "../Style/404.css"; 
+import "../Style/404.css";
+import Footer from "./Footer";
 function P404() {
-    return(
-       <section className="P404Section"> 
-       
-       <div className="NotFoundImage"> 
-         <img src="" alt="NotFoundTitle" />
-       </div>
-           
-      </section>
-    );
+  return <section className="P404Section">
+    <div className="ImageContinar">
+         <img src="../../src/P404.png" id="ImageNot" alt="P404" />
+    </div>
+    <div className="P404Title">
+        <h1 id="NotH1Tit">Page Not Found</h1>
+        <p id="ParaNot">The page you are looking for doesn't exist or has been moved</p>
+    </div>
+    <Footer />
+  </section>;
 }
 export default P404;
