038e0841a4247fddf57f08949ad7910b28f95e81
dreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-13 15:09:38 +0300

    ads

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -17,8 +17,44 @@ function Home() {
   };
 
   type KarType = {
-     
-  }
+    karImage: ReactNode;
+    karH3Titl: string;
+    karParagraph: string;
+  };
+  const karData: KarType[] = [
+    {
+      karImage: <i className="bxr  bx-truck" id="karIcoID"></i>,
+      karH3Titl: "Free Standard Delivery",
+      karParagraph:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, est?",
+    },
+    {
+      karImage: <i className="bxr  bx-credit-card-alt" id="karIcoID"></i>,
+      karH3Titl: "Safe Payment",
+      karParagraph:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, est?",
+    },
+    {
+      karImage: (
+        <i className="bxr  bx-message-circle-question-mark" id="karIcoID"></i>
+      ),
+      karH3Titl: "24/7 Help Center",
+      karParagraph:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, est?",
+    },
+  ];
+
+  const karDateLoop = karData.map((element, index) => {
+    return (
+      <div className="karCard" key={index}>
+        <div className="karImage">{element.karImage}</div>
+        <div className="karTextsCon">
+          <h3 id="karIdH3">{element.karH3Titl}</h3>
+          <p id="karParag">{element.karParagraph}</p>
+        </div>
+      </div>
+    );
+  });
 
   const CategoryCardInfo: CategoryType[] = [
     {
@@ -147,27 +183,12 @@ function Home() {
         <h1 className="CateTitle">Top categories</h1>
         <div className="FlexCArdCAtegory">{LoopCategory}</div>
       </div>
-      
 
       <div className="TabsCardContinar">
         <TABS />
       </div>
 
-      <div className="PRICECON">
-
-        <div className="karCard">
-          <div className="karImage"> 
-            <i className='bxr  bx-truck' id="karIcoID"  ></i> 
-          </div>
-          <div className="karTextsCon">
-            <h3 id="karIdH3">Free Standard Delivery</h3>
-            <p id="karParag">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, est?</p>
-          </div>
-        </div>
-
-      </div>
-
- 
+      <div className="PRICECON">{karDateLoop}</div>
     </section>
   );
 }
