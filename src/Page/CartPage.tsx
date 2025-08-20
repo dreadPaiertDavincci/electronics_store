c6dfe55885e96f1ef34ca3d5cffd64bbf415426c
DreadPaiertDavincci <dreadpaiertdavinci@gmail.com>
2025-08-20 23:38:14 +0300

    sad

--- src/Page/CartPage.tsx
+++ src/Page/CartPage.tsx
@@ -5,6 +5,8 @@ function CartPage() {
     <section className="cartPageSection">
       <div className="InfoCon">
         <div className="ContinarCardCart">
+
+
           <div className="cartCard">
             <div className="CartImage">
               <img
@@ -32,7 +34,272 @@ function CartPage() {
               <IoIosRemoveCircleOutline />
             </div>
           </div>
+
+           <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+ <div className="cartCard">
+            <div className="CartImage">
+              <img
+                id="cartimage"
+                src="../../src/HomeImage/C1.png"
+                alt="tahiyati"
+              />
+            </div>
+            <div className="cartCardBody">
+              <h2 id="cartTi">Airpod</h2>
+              <p id="cartPa">Lorem ipsum dolor sit amet.</p>
+              <p id="cartStar">⭐⭐⭐⭐</p>
+              <div className="PriceContinar">
+                <p className="PriceCart">Price: 000$</p>
+                <input
+                  type="number"
+                  name=""
+                  defaultValue={1}
+                  min={1}
+                  id="cartCardFieldId"
+                />
+              </div>
+            </div>
+            <div className="SilConti">
+              <IoIosRemoveCircleOutline />
+            </div>
+          </div>
+
+
+
         </div>
+
+
         <div className="ButtTotalContinar">
           <h1 id="TOID">Total Price</h1>
           <p id="priceToT">0000$</p>
