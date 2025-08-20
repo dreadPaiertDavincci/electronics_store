f1392587297757e569ba157d50a044a6c76bdc8d
FuturEngineer0 <230404920@st.biruni.edu.tr>
2025-08-20 20:34:34 +0300

    edit

--- src/Com/ProductCard.tsx
+++ src/Com/ProductCard.tsx
@@ -10,13 +10,18 @@ type ProductCardProps = {
 
 const ProductCard = ({ id, image, title, price }: ProductCardProps) => {
   return (
-    <Link to={`/product/${id}`} className="product-link">
-      <div className="product-card">
-        <img src={image} alt={title} className="product-image" />
+  <Link to={`/product/${id}`} className="product-link">
+  <div className="product-card">
+    <img src={image} alt={title} className="product-image" />
+    <div className="product-info-wrapper">
+      <div className="product-text">
         <h3 className="product-title">{title}</h3>
         <p className="product-price">{price}</p>
       </div>
-    </Link>
+      <div className="product-arrow">→</div> 
+    </div>
+  </div>
+</Link>
   );
 };
 
